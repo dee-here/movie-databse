7b8ca7b035ebbf3c8d58a94344edde003bad43d8
Minami Itsukaichi <mitukaichi@gmail.com>
2023-10-30 13:41:48 -0700

    add get movies api

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 const express = require('express');
 // const path = require('path');
-const fs = require ('fs');
+// const fs = require ('fs');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //parse json and url encoded requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      database: 'movies_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+  );
 
 
 // It's done when a `GET` request to the `/api/movies` route renders a list of all movies.
+
+app.get(`/api/movies`, (req, res) => 
+    db.query("SELECT * FROM movies;", function(err, result) {
+        if(result) {
+            console.log(result);
+            res.json(result);
+        }
+        if(err){
+            res.json(err);
+        }
+    })
+);
 
 // It's done when a `GET` request to the `/api/movie-reviews` route renders a list of all reviews with movie data.
 
@@ -21,3 +43,5 @@ app.use(express.urlencoded({ extended: true }));
 // It's done when a `PUT` request to the `/api/review/:id` route successfully updates a movie review when tested using Insomnia.
 
 // It's done when a `DELETE` request to `/api/movie/:id` route deletes a movie when tested using Insomnia.
+
+app.listen(PORT, () => console.log(`app listening at ${PORT}`));
