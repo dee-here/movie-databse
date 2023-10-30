df124278d1ef45c4ff635325860e0507496da260
Minami Itsukaichi <mitukaichi@gmail.com>
2023-10-30 13:05:28 -0700

    add comment to server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// It's done when a `GET` request to the `/api/movies` route renders a list of all movies.
+
+// It's done when a `GET` request to the `/api/movie-reviews` route renders a list of all reviews with movie data.
+
+// It's done when a `POST` request to the `/api/add-movie` route successfully adds a movie when tested using Insomnia.
+
+// It's done when a `PUT` request to the `/api/review/:id` route successfully updates a movie review when tested using Insomnia.
+
+// It's done when a `DELETE` request to `/api/movie/:id` route deletes a movie when tested using Insomnia.
