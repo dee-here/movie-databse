01393d7d478562dd21a8f603102ff2bb32a0f241
Dee <deepakdilse@gmail.com>
2023-10-30 13:23:09 -0700

    add schema and seed

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO movies (movie_name)
+VALUES  ('The Ring'),
+        ('Ring 2'),
+        ('RING 3');
+
+INSERT INTO review (movie_id, review)
+VALUES  (1, 'Best horror yet'),
+        (2, 'Not good'),
+        (3, 'Terrible');
