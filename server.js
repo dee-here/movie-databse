b6bfa30b745e84b628fddec792f719955ad8c1e8
Dee <deepakdilse@gmail.com>
2023-10-30 13:26:32 -0700

    add starter code for server.js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+const express = require('express');
+// const path = require('path');
+const fs = require ('fs');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//parse json and url encoded requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
 // It's done when a `GET` request to the `/api/movies` route renders a list of all movies.
 
 // It's done when a `GET` request to the `/api/movie-reviews` route renders a list of all reviews with movie data.
