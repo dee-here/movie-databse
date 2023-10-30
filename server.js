9d086106a3f2265550066101078870dddd74af46
Minami Itsukaichi <mitukaichi@gmail.com>
2023-10-30 13:56:13 -0700

    add post movie

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ const db = mysql.createConnection(
     console.log(`Connected to the movies_db database.`)
   );
 
-
 // It's done when a `GET` request to the `/api/movies` route renders a list of all movies.
 
 app.get(`/api/movies`, (req, res) => 
@@ -39,6 +38,18 @@ app.get(`/api/movies`, (req, res) =>
 // It's done when a `GET` request to the `/api/movie-reviews` route renders a list of all reviews with movie data.
 
 // It's done when a `POST` request to the `/api/add-movie` route successfully adds a movie when tested using Insomnia.
+app.post('/api/add-movie', (req, res) => {
+    const movieToAdd = req.body.movie;
+    db.query("INSERT INTO movies (movie_name) VALUES (?);", [movieToAdd], function (err, result) {
+        if (result) {
+            res.json(`${movieToAdd} was successfully added`);
+        }
+        if (err) {
+            res.json(err);
+        }
+    })
+})
+
 
 // It's done when a `PUT` request to the `/api/review/:id` route successfully updates a movie review when tested using Insomnia.
 
