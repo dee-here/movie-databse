6979484fd5c92bd9ea37e5198d10746da9ed186b
Dee <deepakdilse@gmail.com>
2023-10-30 15:31:08 -0700

    add get reviews and PUT reviews

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 // Connect to database
 const db = mysql.createConnection(
     {
-      host: '127.0.0.1',
+    //   host: '127.0.0.1',
+      host: 'localhost',
       user: 'root',
-      password: '',
+      //password: '',
+      password: 'password',
       database: 'movies_db'
     },
     console.log(`Connected to the movies_db database.`)
@@ -36,6 +38,19 @@ app.get(`/api/movies`, (req, res) =>
 );
 
 // It's done when a `GET` request to the `/api/movie-reviews` route renders a list of all reviews with movie data.
+app.get(`/api/movie-reviews`, (req, res) => {
+    db.query("SELECT movie_name, review FROM movies JOIN review ON movies.id = review.movie_id;", (err, result) => {
+        if (result) {
+            console.log("result ", result);
+            res.json(result);
+        }
+        if(err) {
+            console.log('Error: ', err);
+            res.json(`Error: ${err}`);
+        }
+    })
+})
+
 
 // It's done when a `POST` request to the `/api/add-movie` route successfully adds a movie when tested using Insomnia.
 app.post('/api/add-movie', (req, res) => {
@@ -52,6 +67,22 @@ app.post('/api/add-movie', (req, res) => {
 
 
 // It's done when a `PUT` request to the `/api/review/:id` route successfully updates a movie review when tested using Insomnia.
+app.put(`/api/review/:id`, (req, res) => {
+    console.log("req is: ", req);
+    // exytract review and review id from request !!  use that in the query below
+    const id = req.params.id;
+    const reviewToUpdate = req.body.review;
+    console.log(req, res);
+    db.query(`UPDATE review SET review=? WHERE id=?;`, [review, id], (err, result) => {
+        if(err) {
+            res.json(`Error : ${err}`);
+        }
+        if(result) {
+            res.json(` Review id: ${id} successfully updated`);
+        }
+    })
+})
+
 
 // It's done when a `DELETE` request to `/api/movie/:id` route deletes a movie when tested using Insomnia.
 
